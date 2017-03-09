12c3748b4e68fdce0c87d9d82e5a0b6036e86bbf
EBaleLa <wasinee.sup@mail.pbru.ac.th>
2017-03-09 18:49:04 +0700

    Update ShiftBit.cpp

--- codes/ShiftBit.cpp
+++ codes/ShiftBit.cpp
@@ -6,12 +6,12 @@ void testBit(int d) {
 	int x = (1<<3);
 	int a = (d & 1);
 	cout << " Test Shift " << d;
-	cout << " \n Test bit 3 = 1 is " << ((d & x)==x)<<endl;
+	cout << " \n Test bit 3 = 1 is " << ((d & x)==x) <<endl;
 	(a==1)?(cout<<" odd "):(cout<<" even ");
 }
 
 main(void) {
-testBit(10);
-getch();
-return 0;
+	testBit(10);
+	getch();
+	return 0;
 }
