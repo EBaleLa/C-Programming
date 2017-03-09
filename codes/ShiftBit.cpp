c19162089fcea04fda3f3ba861733d3ffc83264f
EBaleLa <wasinee.sup@mail.pbru.ac.th>
2017-03-09 18:48:39 +0700

    Update ShiftBit.cpp

--- codes/ShiftBit.cpp
+++ codes/ShiftBit.cpp
@@ -1,7 +1,17 @@
-#include<iostream>
+#include <iostream>
+#include<conio.h>
 using namespace std;
 
-main(void){
-  
+void testBit(int d) {
+	int x = (1<<3);
+	int a = (d & 1);
+	cout << " Test Shift " << d;
+	cout << " \n Test bit 3 = 1 is " << ((d & x)==x)<<endl;
+	(a==1)?(cout<<" odd "):(cout<<" even ");
+}
+
+main(void) {
+testBit(10);
+getch();
 return 0;
 }
